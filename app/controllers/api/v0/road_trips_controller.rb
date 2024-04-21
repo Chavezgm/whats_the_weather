21644e1d60da318975e3a8aab6720fe9103c8d7a
Martin Chavez <martinchavez1203@gmail.com>
2024-04-21 17:27:50 -0600

    modify conditional if destination is impossible to get to

---
+++ app/controllers/api/v0/road_trips_controller.rb
@@ -0,0 +1,24 @@
+class Api::V0::RoadTripsController < ApplicationController
+  def create
+    # require 'pry'; binding.pry
+    user = User.find_by(api_key: roadtrip_params[:api_key])
+    if user 
+      road_trip = RoadtripSerializer.serialize_roadtrip(roadtrip_params[:origin], roadtrip_params[:destination])
+      # require 'pry'; binding.pry
+      if road_trip.is_a?(Hash) && road_trip[:error].present?
+        render json: {errors: "Impossible"}, status: :not_acceptable
+      else 
+        render json: road_trip
+      end
+
+    else 
+      render json: { errors: 'Invalid api ket, try again!!!'} , status: :unauthorized
+    end
+  end
+
+  private 
+  def roadtrip_params
+    payload = JSON.parse(request.body.read, symbolize_names: true)
+    ActionController::Parameters.new(payload).permit(:origin, :destination, :api_key)
+  end
+end
