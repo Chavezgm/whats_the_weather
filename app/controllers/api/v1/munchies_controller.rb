cabcda9f37c7538c5da3fdc6041602d8c8a108b4
Martin Chavez <martinchavez1203@gmail.com>
2024-04-23 15:00:02 -0600

    fix params

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MunchiesController < ApplicationController
   def show
     # require 'pry'; binding.pry
-    loc = params[:location]
+    loc = params[:destination]
     cuisine = params[:food]
 
     render json: MunchiesSerializer.new.serialize(loc, cuisine)
